1b2739d90cf61ff414859c1f5e87564a99c63e43
T&T Development <99367973+terrathedeveloper@users.noreply.github.com>
2022-07-26 08:40:42 -0500

    Update firstApp.R

--- firstApp.R
+++ firstApp.R
@@ -1,6 +1,9 @@
 install.packages(c("shiny","shinythemes"))
 library(shiny)
 library(shinythemes)
+library(ggplot2)
+library(dplyr)
+Pokemon <- read.csv("C:/Users/terra/projects/ColumbiaUniversity/CourseLessonPlans/WebApps/pokemon.csv")
 
 
 ui<- fluidPage(
@@ -12,6 +15,7 @@ ui<- fluidPage(
             tags$h3("Input:"),
             textInput("txt1","Pokemon Name","")
           ),
+       
           mainPanel(
             h1("HP Chart"),
             #verbatimTextOutput("txtout")
@@ -28,14 +32,25 @@ ui<- fluidPage(
                     #verbatimTextOutput("txtout")
                     plotOutput(outputId = "waterPokemon")
                   )    
+      ),
+      tabPanel("All Pokemon",
+               sidebarPanel(
+                 tags$h3("Choose a Type"),
+                  selectInput("type", label="Pokemon Type", choices =rownames(table(Pokemon$Type.1)), selected="Fire")#create a list of choices based on pokemon types )
+               ),
+               mainPanel(
+                 h1("HP Chart"),
+                 #verbatimTextOutput("txtout")
+                 plotOutput(outputId = "allPokemon")
+               )    
       )
     )          
 )
 
 server<- function(input, output){
-  Pokemon <- read.csv("C:/Users/terra/projects/ColumbiaUniversity/CourseLessonPlans/WebApps/pokemon.csv")
   Pokemon %>% filter(Pokemon$Type.1=="Fire")->fire_pokemon
   Pokemon %>% filter(Pokemon$Type.1=="Water")->water_pokemon
+ 
   
   output$firePokemon <- renderPlot({
     ggplot(data = fire_pokemon, aes(x=HP,y=Speed))+geom_point(color="red")
@@ -45,6 +60,10 @@ server<- function(input, output){
     ggplot(data = water_pokemon, aes(x=HP,y=Speed))+geom_point(color="blue")
   })
   
+  output$allPokemon<- renderPlot({
+    Pokemon %>% filter(Pokemon$Type.1==input$type)->all_pokemon
+    ggplot(data=all_pokemon, aes(x=Attack, y=Defense))+ geom_point(color="orange")
+  })
   
   output$txtout <- renderText({
     paste(input$txt1)
